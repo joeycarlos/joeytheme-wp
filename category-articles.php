f071ded4ad2cb6346e09dc14f89faa12c6541275
joeycarlos <jclaguio@gmail.com>
2016-03-13 16:23:07 -0700

    Article template post listing

--- category-articles.php
+++ category-articles.php
@@ -4,23 +4,26 @@ get_header();
 
 ?> 
 
-ARTICLES CATEGORY PAGE
-
-<ul>
+<table>
 <?php
 
 $args = array( 'category_name' => 'articles' );
 $myposts = get_posts( $args );
 foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
-	<li>
-		<a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a>
-	</li>
-
+	<tr>
+		<?php $dateString = get_the_date('M j'); ?>
+		<td class="post-date-cell">
+			<h3 class="post-date"><?php echo strtoupper($dateString); ?></h3>
+		</td>
+		<td class="post-title-cell">
+			<h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		</td>
+	</tr>
 <?php endforeach;
 wp_reset_postdata();
 
 ?>
-</ul>
+</table>
 
 <?php
 
--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if (is_home()) :
 				<?php $dateString = get_the_date('M j'); ?>
 					<td class="post-date-cell"><h3 class="post-date"><?php echo strtoupper($dateString); ?></h3></td>
 					<td class="post-title-cell"><h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2></td>
-				</td>
+				</tr>
 			<?php }
 		
 		endwhile;
