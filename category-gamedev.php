30959d372a8ed9167be5d7174618ac37e8705c12
joeycarlos <jclaguio@gmail.com>
2016-03-13 16:26:35 -0700

    Add post listings to all category templates

--- category-gamedev.php
+++ category-gamedev.php
@@ -4,7 +4,26 @@ get_header();
 
 ?> 
 
-GAMEDEV CATEGORY PAGE
+<table>
+<?php
+
+$args = array( 'category_name' => 'gamedev' );
+$myposts = get_posts( $args );
+foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
+	<tr>
+		<?php $dateString = get_the_date('M j'); ?>
+		<td class="post-date-cell">
+			<h3 class="post-date"><?php echo strtoupper($dateString); ?></h3>
+		</td>
+		<td class="post-title-cell">
+			<h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		</td>
+	</tr>
+<?php endforeach;
+wp_reset_postdata();
+
+?>
+</table>
 
 <?php
 
