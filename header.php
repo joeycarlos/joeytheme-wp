f1bf07a98694642c465222d3fe464dc5534d541a
joeycarlos <jclaguio@gmail.com>
2016-03-12 21:01:37 -0800

    Add profile picture and temporary layout adjustments

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
 
 		<!-- site-header -->
 		<header class="site-header">
+			<img class="header-picture" src="/wordpress/wp-content/themes/joeytheme/profile_picture.png" style="width:150px;height:150px">
 			<h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
 
 			<nav class="site-nav">
