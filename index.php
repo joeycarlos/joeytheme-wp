224574d0ad67c2f8a72e6742e0c398f335cc7fae
joeycarlos <jclaguio@gmail.com>
2016-03-13 16:11:28 -0700

    Add personal category template and exclude posts from homepage

--- index.php
+++ index.php
@@ -8,13 +8,16 @@ if (is_home()) :
 
 	if (have_posts()) :
 
-		while (have_posts()) : the_post(); ?>
-			<tr>
-			<?php $dateString = get_the_date('M j'); ?>
-				<td class="post-date-cell"><h3 class="post-date"><?php echo strtoupper($dateString); ?></h3></td>
-				<td class="post-title-cell"><h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2></td>
-			</td>
-		<?php 
+		while (have_posts()) : the_post();
+
+			if ( !in_category('personal') )  { ?>
+				<tr>
+				<?php $dateString = get_the_date('M j'); ?>
+					<td class="post-date-cell"><h3 class="post-date"><?php echo strtoupper($dateString); ?></h3></td>
+					<td class="post-title-cell"><h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2></td>
+				</td>
+			<?php }
+		
 		endwhile;
 
 		else :
