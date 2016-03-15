c394ebc1662af5e6c38fbc89c222472d4a0e16b2
joeycarlos <jclaguio@gmail.com>
2016-03-15 11:39:44 -0700

    Header about picture link and single post page styling

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
 		<!-- site-header -->
 		<header class="site-header">
 		<div class="site-banner">
-			<a href="#" class="picture-about-link"><img class="header-picture" src="/wordpress/wp-content/themes/joeytheme/images/profile_picture.png"></a>
+			<a href="//localhost/wordpress/about" class="picture-about-link"><img class="header-picture" src="/wordpress/wp-content/themes/joeytheme/images/profile_picture.png"></a>
 			<h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
 			<p class="site-description"><?php bloginfo('description'); ?></p>
 		</div>
--- single-post.php
+++ single-post.php
@@ -5,17 +5,18 @@
 global $post;
 $slug = get_post( $post )->post_name;
 $query = new WP_Query( array('name' => $slug) );
+
 ?>
 
 <?php
 if ( $query->have_posts() ) :
 	while ( $query->have_posts() ) : $query->the_post(); ?>
 		<div class="post">
-			<h2><a href="<?php the_permalink() ?>" 
+			<h2 class = "post-page-title"><a href="<?php the_permalink() ?>" 
 				title="<?php the_title_attribute()?>" >
 				<?php the_title(); ?>
 			</a></h2>
-			<p class="post-content"> <?php the_content() ?> </p>
+			<p class="post-page-content"> <?php the_content() ?> </p>
 		</div> <?php 
 	endwhile;
 else:
