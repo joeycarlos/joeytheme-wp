872f8ae9c7d3326277fd398d3100de62dc97bcba
joeycarlos <jclaguio@gmail.com>
2016-03-17 16:02:17 -0700

    Add about page info

--- page-about.php
+++ page-about.php
@@ -1,9 +1,119 @@
 <?php get_header(); ?> 
 
 <?php 
+/*
 $page = get_page_by_title('about');
 $content = apply_filters('the_content', $page->post_content); 
 echo $content;  
+*/
 ?>
 
+<h3 class="about-page-title"> About </h3>
+<img class="about-picture" src="/wordpress/wp-content/themes/joeytheme-wp/images/family-picture.jpg">
+
+<table class="about-table">
+	<tr class="about-row">
+		<td class="about-field-title"> Full Name: </td>
+		<td class="about-field"> Jose-Carlos (Joey) Peralta Laguio </td>
+	</tr>
+	<tr class="about-row">
+		<td class="about-field-title"> Occupation: </td>
+		<td class="about-field"> Music Teacher at CW Music </td>
+	</tr>
+	<tr class="about-row">
+		<td class="about-field-title"> Previous Employment: </td>
+		<td class="about-field">
+			<br> QA Engineer, Mobify, 2015
+			<br> QA Analyst, Appnovation Technologies, 2013
+			<br> QA Engineer, Broadcom Corporation, 2012
+		</td>
+	</tr>
+	<tr class="about-row">
+		<td class="about-field-title"> Musical Experience: </td>
+		<td class="about-field">
+			<br> Corpus Christi College Chamber Choir, 2010-Present
+			<br> 8Wings Vocal Ensemble, 2014-Present
+			<br> UBC Laptop Orchestra, 2014-2015
+			<br> Right Here, Write Now! The Musical, 2014
+			<br> UBC A Cappella, 2013-2015
+			<br> St. Patrick's Chamber Choir, 2006-2010
+		</td>
+	</tr>
+	<tr class="about-row">
+		<td class="about-field-title"> Post Secondary Education: </td>
+		<td class="about-field"> Bachelor's of Applied Science in Software Engineering, 
+		<br> University of British Columbia, 2015
+		</td>
+	</tr>
+	<tr class="about-row">
+		<td class="about-field-title"> High School Education: </td>
+		<td class="about-field"> St. Patrick's Regional Secondary School, 2010
+		</td>
+	</tr>
+	<tr class="about-row">
+		<td class="about-field-title"> Elementary School Education: </td>
+		<td class="about-field"> St. Paul's School, 2005
+		</td>
+	</tr>
+	<tr class="about-row">
+		<td class="about-field-title"> Awards and Accomplishments: </td>
+		<td class="about-field"> UBC Engineering Dean's List, 2010-2015
+			<br>High School Valedictorian
+			<br>Top Student in Math, Physics, Chemistry, and Choral Music
+			<br>Dogwood Scholarship for Voice Performance
+		</td>
+	</tr>
+	<tr class="about-row">
+		<td class="about-field-title"> Location: </td>
+		<td class="about-field"> Vancouver, BC, Canada </td>
+	</tr>
+	<tr class="about-row">
+		<td class="about-field-title"> Age: </td>
+		<td class="about-field"> 23 </td>
+	</tr>
+	<tr class="about-row">
+		<td class="about-field-title"> Religion: </td>
+		<td class="about-field"> Christian-Catholic </td>
+	</tr>
+	<tr class="about-row">
+		<td class="about-field-title"> Political Views: </td>
+		<td class="about-field"> Liberal </td>
+	</tr>
+	<tr class="about-row">
+		<td class="about-field-title"> Major Role Models: </td>
+		<td class="about-field"> Socrates, Jesus, Leonardo Da Vinci, Jonathan Blow, Sam Smith, Jose Rizal, Augustus Caesar </td>
+	</tr>
+	<tr class="about-row">
+		<td class="about-field-title"> Lifetime Goals: </td>
+		<td class="about-field">
+			<br>Acquire a PhD in Philosophy from the University of California
+			<br>Work full time as a game software engineer
+			<br>Play piano at an RCM Grade 10 level
+			<br>Live in San Francisco (at least temporarily)
+		</td>
+	</tr>
+	<tr class="about-row">
+		<td class="about-field-title"> Interests and Hobbies: </td>
+		<td class="about-field">
+			<br>Developing and playing video games via DirectX/Unreal
+			<br>Reading philosophical and historical texts
+			<br>Music - especially singing, piano, and guitar
+			<br>Developing blogs using Wordpress
+			<br>Video Game Development
+		</td>
+	</tr>
+	<tr class="about-row">
+		<td class="about-field-title"> Musical Influences: </td>
+		<td class="about-field"> Donny Hathaway, Sam Cooke, Luther Vandross, Sam Smith, Michael Jackson, Jessie J, Mariah Carey, Beyonce, Earth Wind and Fire, Boyz II Men, Brian McKnight, Gary Valenciano, Babyface </td>
+	</tr>
+	<tr class="about-row">
+		<td class="about-field-title"> Favourite Games: </td>
+		<td class="about-field"> Guild Wars, Guild Wars 2, Call of Duty, The Elder Scrolls: Oblivion, GTA: Vice City, GTA 5, The Witness, Age of Mythology, Age of Empires: Rise of Rome,  Risk Your Life: Online </td>
+	</tr>
+	<tr class="about-row">
+		<td class="about-field-title"> Computer Specs: </td>
+		<td class="about-field"> 16GB RAM Intel i7 Processor, GTX 970 2GB Graphics, 512GB SSD, 2TB DDR, ASUS 27" Monitor, HP 24" Monitor, Bose 2.1 Speaker System, Alienware PC, Razer BlackWidow Ultimate Keyboard, Razer Chroma Mouse </td>
+	</tr>
+
+</table>
 <?php get_footer(); ?>
