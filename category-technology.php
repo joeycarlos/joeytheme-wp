e7b0a5912363330855d95f6e0cc421b182867da9
joeycarlos <jclaguio@gmail.com>
2016-03-18 14:43:19 -0700

    Add pagination to category pages

--- category-technology.php
+++ category-technology.php
@@ -7,19 +7,26 @@
   <?php echo category_description(); ?>
 </div>
 
-<table>
+
 <?php
 $prevYear = 2000;
 $prevDay = 2000;
 
 $catslugs = array('technology');
 $catids = get_cats_by_slug($catslugs);
-$args = array( 'category__in' => $catids );
+
+$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+
+$args = array( 'category__in' => $catids,
+			'posts_per_page' => get_option(posts_per_page),
+			'paged' => $paged
+		 );
 
 $query = new WP_Query( $args );
 
-if ( $query->have_posts() ) :
-
+if ( $query->have_posts() ) : ?>
+<table>
+<?php
 while( $query->have_posts() ) : $query->the_post(); ?>
 	<tr>
 		<?php $dateString = get_the_date('M j'); ?>
@@ -35,8 +42,37 @@ while( $query->have_posts() ) : $query->the_post(); ?>
 	</tr>
 	<?php $prevYear = $currentPostYear; ?>
 	<?php $prevDay = $currentPostDay; ?>
-<?php endwhile;
-wp_reset_postdata();
+<?php endwhile; ?>
+
+</table>
+
+	<?php
+
+	// PAGINATION
+	// NOTE: The number of posts per page is set according to what is listed in Settings/Reading 
+	$temp_query = $wp_query;
+	$wp_query = NULL;
+	$wp_query = $query;
+
+	if ($query->max_num_pages > 1) { // check if the max number of pages is greater than 1  ?>
+		<div class="prev-next-posts">
+			<p class="prev-posts-link">
+		      <?php echo get_previous_posts_link( '<<' ); 
+		      // display newer posts link ?>
+		    </p>
+		    <p class="current-page-number"> <?php echo $paged ?>
+		    </p>
+		    <p class="next-posts-link">
+		      <?php echo get_next_posts_link( '>>', $query->max_num_pages ); 
+		      // display older posts link ?>
+		    </p>
+	    </div>
+	<?php } 
+	$wp_query = NULL;
+	$wp_query = $temp_query;
+	// END PAGINATION
+
+	wp_reset_postdata();
 
 else :
 	echo '<p>No content found</p>';
@@ -44,6 +80,6 @@ else :
 endif;
 
 ?>
-</table>
+
 
 <?php get_footer(); ?>
