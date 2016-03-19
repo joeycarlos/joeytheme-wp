63eea4b4ce10a7790947ea8be1b05031063b1777
joeycarlos <jclaguio@gmail.com>
2016-03-19 13:59:23 -0700

    Refactor single-post page template

--- single-post.php
+++ single-post.php
@@ -1,42 +1,47 @@
-<?php get_header(); ?> 
-
-<?php
+<?php get_header();
 
 global $post;
 $slug = get_post( $post )->post_name;
 $query = new WP_Query( array('name' => $slug) );
 
-?>
-
-<?php
 if ( $query->have_posts() ) :
+
 	while ( $query->have_posts() ) : $query->the_post(); ?>
+
 		<div class="post">
 			<div class="post-banner">
-			<h2 class = "post-page-title"><a href="<?php the_permalink() ?>" 
-				title="<?php the_title_attribute()?>" >
-				<?php the_title(); ?>
-			</a></h2>
-			<?php if ( in_category('technology') ) { ?>
-				<i class="fa fa-cogs fa-lg" class="post-page-icon"></i>
-				<?php } ?>
-			<?php if ( in_category('essays') ) { ?>
-				<i class="fa fa-pencil fa-lg" class="post-page-icon"></i>
-				<?php } ?>
-			<?php if ( in_category('art') ) { ?>
-				<i class="fa fa-music fa-lg" class="post-page-icon"></i>
-				<?php } ?>
-			<p class="post-page-date">
-				<?php the_date('Y M j - g:i A'); ?>
-			</p>
+				<h2 class = "post-page-title"><a href="<?php the_permalink() ?>" 
+					title="<?php the_title_attribute()?>" >
+					<?php the_title(); ?></a>
+				</h2>
+
+				<?php if ( in_category('technology') ) { ?>
+					<i class="fa fa-cogs fa-lg" class="post-page-icon"></i>
+					<?php } ?>
+
+				<?php if ( in_category('essays') ) { ?>
+					<i class="fa fa-pencil fa-lg" class="post-page-icon"></i>
+					<?php } ?>
+
+				<?php if ( in_category('art') ) { ?>
+					<i class="fa fa-music fa-lg" class="post-page-icon"></i>
+					<?php } ?>
+
+				<p class="post-page-date">
+					<?php the_date('Y M j - g:i A'); ?>
+				</p>
 
 			</div>
 
 			<?php the_content(); ?> 
-		</div> <?php 
+		</div> 
+
+	<?php 
 	endwhile;
+
 else:
 	echo ('Sorry. No matching content was found.');
+
 endif;
 ?>
 
