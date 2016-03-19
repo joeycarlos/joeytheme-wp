cbf0495095628ba0f468a4dad5d28f53c1fa887c
joeycarlos <jclaguio@gmail.com>
2016-03-19 13:55:36 -0700

    Refactor index page

--- index.php
+++ index.php
@@ -14,91 +14,93 @@ $query = new WP_Query( $args );
 if (is_home()) :
 	if ( $query->have_posts() ) : 
 
-	$prevYear = 2000;
-	$prevDay = 2000;
-	?>
-
-	<table> 
-	<?php
-	while( $query->have_posts() ) : $query->the_post(); ?>
-
-		<?php 
-		$dateString = get_the_date('M j');
-		$currentPostYear = get_the_date('Y');
-		$currentPostDay = get_the_date('M j Y'); 
+		$prevYear = 2000;
+		$prevDay = 2000;
 		?>
 
-		<tr>
-			<td class="post-year-cell"><h3 class="post-year">
-				<?php 
-				if ($prevYear != $currentPostYear) { 
-					echo $currentPostYear; }?>
-			</h3></td>
+		<table> 
+		<?php
+		while( $query->have_posts() ) : $query->the_post(); ?>
 
-			<td class="post-date-cell"><h3 class="post-date">
-				<?php 
-				if ($prevDay != $currentPostDay ) { 
-					echo strtoupper($dateString); } ?>
-			</h3></td>
+			<?php 
+			$dateString = get_the_date('M j');
+			$currentPostYear = get_the_date('Y');
+			$currentPostDay = get_the_date('M j Y'); 
+			?>
 
-			<td class="post-category-cell">
-				<?php 
-				if ( in_category('technology') ) { ?>
-					<i class="fa fa-cogs fa-lg" class="main-game-icon"></i>
-				<?php } ?>
+			<tr>
+				<td class="post-year-cell"><h3 class="post-year">
+					<?php 
+					if ($prevYear != $currentPostYear) { 
+						echo $currentPostYear; }?>
+				</h3></td>
 
-				<?php 
-				if ( in_category('essays') ) { ?>
-					<i class="fa fa-pencil fa-lg" class="main-article-icon"></i>
-				<?php } ?>
+				<td class="post-date-cell"><h3 class="post-date">
+					<?php 
+					if ($prevDay != $currentPostDay ) { 
+						echo strtoupper($dateString); } ?>
+				</h3></td>
 
-				<?php 
-				if ( in_category('art') ) { ?>
-					<i class="fa fa-music fa-lg" class="main-music-icon"></i>
-				<?php } ?>
-			</td>
+				<td class="post-category-cell">
+					<?php 
+					if ( in_category('technology') ) { ?>
+						<i class="fa fa-cogs fa-lg" class="main-game-icon"></i>
+					<?php } ?>
 
-			<td class="post-title-cell"><h2 class="post-title"><a href="<?php the_permalink(); ?>">
-				<?php 
-				the_title(); ?>
-				</a></h2></td>
-		</tr>
+					<?php 
+					if ( in_category('essays') ) { ?>
+						<i class="fa fa-pencil fa-lg" class="main-article-icon"></i>
+					<?php } ?>
+
+					<?php 
+					if ( in_category('art') ) { ?>
+						<i class="fa fa-music fa-lg" class="main-music-icon"></i>
+					<?php } ?>
+				</td>
+
+				<td class="post-title-cell"><h4 class="post-title"><a href="<?php the_permalink(); ?>">
+					<?php 
+					the_title(); ?>
+					</a></h2></td>
+			</tr>
+
+			<?php 
+			$prevYear = $currentPostYear;
+			$prevDay = $currentPostDay; 
+
+		endwhile;
+		?>
+		</table> 
+
+		<?php
+		
+		$temp_query = $wp_query;
+		$wp_query = NULL;
+		$wp_query = $query;
+
+		if ($query->max_num_pages > 1) : 
+		?>
+			<div class="prev-next-posts">
+				<p class="prev-posts-link">
+			      <?php echo get_previous_posts_link( '<<' ); ?>
+			    </p>
+
+			    <p class="current-page-number">
+			    	<?php echo $paged ?>
+			    </p>
+
+			    <p class="next-posts-link">
+			      <?php echo get_next_posts_link( '>>', $query->max_num_pages ); ?>
+			    </p>
+		    </div>
 
 		<?php 
-		$prevYear = $currentPostYear;
-		$prevDay = $currentPostDay; 
+		endif;
 
-	endwhile;
-	?>
-	</table> 
+		$wp_query = NULL;
+		$wp_query = $temp_query;
 
-	<?php
-	
-	// PAGINATION
-	// NOTE: The number of posts per page is set according to what is listed in Settings/Reading 
-	$temp_query = $wp_query;
-	$wp_query = NULL;
-	$wp_query = $query;
-
-	if ($query->max_num_pages > 1) { // check if the max number of pages is greater than 1  ?>
-		<div class="prev-next-posts">
-			<p class="prev-posts-link">
-		      <?php echo get_previous_posts_link( '<<' ); 
-		      // display newer posts link ?>
-		    </p>
-		    <p class="current-page-number"> <?php echo $paged ?>
-		    </p>
-		    <p class="next-posts-link">
-		      <?php echo get_next_posts_link( '>>', $query->max_num_pages ); 
-		      // display older posts link ?>
-		    </p>
-	    </div>
-	<?php } 
-	$wp_query = NULL;
-	$wp_query = $temp_query;
-	// END PAGINATION
-
-	wp_reset_postdata();
+		wp_reset_postdata();
 
 	else :
 		echo '<p>No content found</p>';
