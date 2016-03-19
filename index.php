68cffc8b5f4cc045e961d1d928abc865bd92cef2
joeycarlos <jclaguio@gmail.com>
2016-03-19 11:24:48 -0700

    Refactor index partially

--- index.php
+++ index.php
@@ -1,57 +1,77 @@
-<?php get_header(); ?> 
+<?php get_header(); ?>
 
 <?php
 
-$prevYear = 2000;
-$prevDay = 2000;
-
 $catslugs = array('technology','essays','art');
 $catids = get_cats_by_slug($catslugs);
-
 $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-
 $args = array( 'category__in' => $catids,
 			'posts_per_page' => get_option(posts_per_page),
 			'paged' => $paged
 		 );
-
 $query = new WP_Query( $args );
 
 if (is_home()) :
+	if ( $query->have_posts() ) : 
 
-	if ( $query->have_posts() ) : ?>
+	$prevYear = 2000;
+	$prevDay = 2000;
+	?>
 
 	<table> 
 	<?php
 	while( $query->have_posts() ) : $query->the_post(); ?>
+
+		<?php 
+		$dateString = get_the_date('M j');
+		$currentPostYear = get_the_date('Y');
+		$currentPostDay = get_the_date('M j Y'); 
+		?>
+
 		<tr>
-		<?php $dateString = get_the_date('M j'); ?>
-		<?php $currentPostYear = get_the_date('Y'); ?>
-		<?php $currentPostDay = get_the_date('M j Y'); ?>
-			<td class="post-year-cell"><h3 class="post-year"><?php if ($prevYear != $currentPostYear) { echo $currentPostYear; }?>
-				</h3></td>
-			<td class="post-date-cell"><h3 class="post-date"><?php if ($prevDay != $currentPostDay ) { echo strtoupper($dateString); } ?></h3></td>
+			<td class="post-year-cell"><h3 class="post-year">
+				<?php 
+				if ($prevYear != $currentPostYear) { 
+					echo $currentPostYear; }?>
+			</h3></td>
+
+			<td class="post-date-cell"><h3 class="post-date">
+				<?php 
+				if ($prevDay != $currentPostDay ) { 
+					echo strtoupper($dateString); } ?>
+			</h3></td>
+
 			<td class="post-category-cell">
-				<?php if ( in_category('technology') ) { ?>
+				<?php 
+				if ( in_category('technology') ) { ?>
 					<i class="fa fa-cogs fa-lg" class="main-game-icon"></i>
-					<?php } ?>
-				<?php if ( in_category('essays') ) { ?>
+				<?php } ?>
+
+				<?php 
+				if ( in_category('essays') ) { ?>
 					<i class="fa fa-pencil fa-lg" class="main-article-icon"></i>
-					<?php } ?>
-				<?php if ( in_category('art') ) { ?>
+				<?php } ?>
+
+				<?php 
+				if ( in_category('art') ) { ?>
 					<i class="fa fa-music fa-lg" class="main-music-icon"></i>
-					<?php } ?>
+				<?php } ?>
 			</td>
-			<td class="post-title-cell"><h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2></td>
+
+			<td class="post-title-cell"><h2 class="post-title"><a href="<?php the_permalink(); ?>">
+				<?php 
+				the_title(); ?>
+				</a></h2></td>
 		</tr>
-		<?php $prevYear = $currentPostYear; ?>
-		<?php $prevDay = $currentPostDay; ?>
 
-	<?php	
+		<?php 
+		$prevYear = $currentPostYear;
+		$prevDay = $currentPostDay; 
+
 	endwhile;
-
 	?>
-	 </table> 
+	</table> 
+
 	<?php
 	
 	// PAGINATION
@@ -78,22 +98,14 @@ if (is_home()) :
 	$wp_query = $temp_query;
 	// END PAGINATION
 
-
 	wp_reset_postdata();
 
 	else :
 		echo '<p>No content found</p>';
 
-	endif; ?>
-
-
-
-<?php
+	endif;
 
 endif;
-
-?><?php
-
-get_footer();
-
 ?>
+
+<?php get_footer(); ?>
