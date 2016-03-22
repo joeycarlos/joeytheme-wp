9e4cd36916f615f72699c60aa164ee092b6f7ad7
joeycarlos <jclaguio@gmail.com>
2016-03-22 01:06:07 -0700

    Remove all styling. Add responsive header

--- header.php
+++ header.php
@@ -3,7 +3,27 @@
 
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
-	<meta name="viewport" content="width=device-width">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	<script>
+		$(function() {
+			var pull 		= $('#pull');
+				menu 		= $('nav ul');
+				menuHeight	= menu.height();
+
+			$(pull).on('click', function(e) {
+				e.preventDefault();
+				menu.slideToggle();
+			});
+
+			$(window).resize(function(){
+        		var w = $(window).width();
+        		if(w > 320 && menu.is(':hidden')) {
+        			menu.removeAttr('style');
+        		}
+    		});
+		});
+	</script>
 	<title><?php bloginfo('name'); ?></title>
 	<link href='https://fonts.googleapis.com/css?family=Kreon:400,300,700' rel='stylesheet' type='text/css'>
 	<link href='https://fonts.googleapis.com/css?family=Signika' rel='stylesheet' type='text/css'>
@@ -16,26 +36,32 @@
 	<div class="container">
 		<!-- site-header -->
 		<header class="site-header">
-		<div class="site-banner">
-			<a href="<?php bloginfo('url'); ?>/about" class="picture-about-link">
-				<img class="header-picture" 
-					src="<?php bloginfo('url'); ?>/wp-content/themes/joeytheme-wp/images/profile_picture.png" 
-					onmouseover="this.src='<?php bloginfo('url'); ?>/wp-content/themes/joeytheme-wp/images/profile_picture_hover.png'" 
-					onmouseout="this.src='<?php bloginfo('url'); ?>/wp-content/themes/joeytheme-wp/images/profile_picture.png'">
-			</a>
-			<h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
-			<p class="full-name">Jose-Carlos (Joey) Peralta Laguio</p>
-		</div>
 
-		<p class="header-blurb"><?php bloginfo('description'); ?></p>
+			<div class="site-banner">
 
-		<nav class="site-nav">
-			<?php
-				$args = array(
-					'theme_location' => 'primary'
-				);
-			?>
-			<?php echo wp_nav_menu( $args ); ?>
-		</nav>
+				<a href="<?php bloginfo('url'); ?>/about" class="picture-about-link">
+					<img class="header-picture" 
+						src="<?php bloginfo('url'); ?>/wp-content/themes/joeytheme-wp/images/profile_picture.png" 
+						onmouseover="this.src='<?php bloginfo('url'); ?>/wp-content/themes/joeytheme-wp/images/profile_picture_hover.png'" 
+						onmouseout="this.src='<?php bloginfo('url'); ?>/wp-content/themes/joeytheme-wp/images/profile_picture.png'">
+				</a>
+
+				<div class="site-title">
+					<h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+					<p class="full-name">Jose-Carlos (Joey) Peralta Laguio</p>
+				</div>
+
+			</div>
+
+			<p class="header-blurb"><?php bloginfo('description'); ?></p>
+
+			<nav class="site-nav">
+				<?php
+					$args = array(
+						'theme_location' => 'primary'
+					);
+				?>
+				<?php echo wp_nav_menu( $args ); ?>
+			</nav>
 
 		</header><!-- /site-header -->
